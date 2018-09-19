7721f395cc640f6e8c2f5f7bcda1f6be29abd2b6
shotaro-miyazaki <39908393+shotaro-miyazaki@users.noreply.github.com>
2018-09-19 09:42:22 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
 let box1 = Math.floor(Math.random()*3 + 1);
-    document.write("1つ目の数字「"+box1+"」");
+    document.write("first number「"+box1+"」");
 
 let box2 = Math.floor(Math.random()*3 + 1);
-    document.write("2つ目の数字「 "+box2+" 」");
+    document.write("second number「 "+box2+" 」");
 
 let box3 = Math.floor(Math.random()*3 + 1);
-    document.write("3つ目の数字「"+box3+"」");
+    document.write("third number"+box3+"」");
 
     if(box1 == box2 && box1 == box3){
-        document.write("全ての数字が揃った");
+        document.write("All numbers matched!");
     }else if(box1 == box2 || box1 == box3 || box2 == box3 ){
-        document.write("おしい");
+        document.write("Only 2 matched");
     }else{
-        document.write("全然ダメ");
+        document.write("Nothing matched");
     }
 
 window.onload = function () {
